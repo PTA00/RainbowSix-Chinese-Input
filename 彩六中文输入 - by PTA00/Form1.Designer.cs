9cf1bf93cd8df0a70e4b9ff4ecd28424cb5d085a
PTA00 <PTA00@WIN-RJCLIH51B75>
2022-02-25 21:24:23 +0800

    添加项目文件。

---
+++ 彩六中文输入 - by PTA00/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace 彩六中文输入___by_PTA00
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(403, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "发送（请切换到游戏手动打开聊天框）";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10F);
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "消息内容：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 10F);
+            this.textBox1.Location = new System.Drawing.Point(107, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(313, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(339, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "标准.NET应用程序，极为简单，不操作游戏窗口。";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(12, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(270, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "您有3秒的时间进行切换\r\n自动断行后有2秒的时间重新打开聊天栏";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 168);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "彩六中文输入 - by PTA00";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 彩六中文输入 - by PTA00/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Threading.Thread;
+using System.Runtime.InteropServices;
+
+
+namespace 彩六中文输入___by_PTA00
+{
+
+    public partial class Form1 : Form
+    {
+        [StructLayout(LayoutKind.Explicit)]
+        public struct INPUT
+        {
+            [FieldOffset(0)]
+            public int type;
+            [FieldOffset(4)]
+            public KEYBDINPUT ki;
+            [FieldOffset(4)]
+            public MOUSEINPUT mi;
+            [FieldOffset(4)]
+            public HARDWAREINPUT hi;
+        }
+        public struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+        public struct KEYBDINPUT
+        {
+            public short wVk;
+            public short wScan;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+        public struct HARDWAREINPUT
+        {
+            public int uMsg;
+            public short wParamL;
+            public short wParamH;
+        }
+
+        [DllImport("user32")]
+        public static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int i = 127;
+            //冷知识：彩六最多能打127个英文字符或42个中文字符，一个中文字符等于3个英文字符
+            string keys = textBox1.Text;
+            Sleep(3000);
+            
+            foreach (char c in keys)
+            {
+                if (i >= 3)
+                {
+                    if (c > 127)
+                    {
+                        //中文3
+                        i -= 3;
+                    }
+                    else
+                    {
+                        //英文1
+                        i--;
+                    }
+                }
+                else
+                {
+                    SimulateInputKey(0x0D);//ENTER键
+                    Sleep(2000);
+                    
+                    i = 127;
+                }
+                SimulateInputString(c);//发送字符
+                int num = r.Next(50, 151);//随机延时
+                Sleep(num);
+            }
+            SimulateInputKey(0x0D);//ENTER键
+
+        }
+
+
+        public void SimulateInputString(char c)
+        {
+            INPUT[] input = new INPUT[2];
+                
+            input[0].type = 1;
+            input[0].ki.wVk = 0;//dwFlags 为KEYEVENTF_UNICODE 即4时，wVk必须为0
+            input[0].ki.wScan = (short)c;
+            input[0].ki.dwFlags = 4;//输入UNICODE字符
+            input[0].ki.time = 0;
+            input[0].ki.dwExtraInfo = IntPtr.Zero;
+            input[1].type = 1;
+            input[1].ki.wVk = 0;
+            input[1].ki.wScan = (short)c;
+            input[1].ki.dwFlags = 6;
+            input[1].ki.time = 0;
+            input[1].ki.dwExtraInfo = IntPtr.Zero;
+            SendInput(2u, input, Marshal.SizeOf((object)default(INPUT)));
+            
+        }
+
+        public void SimulateInputKey(int key)
+        {
+            INPUT[] input = new INPUT[1];
+
+            input[0].type = 1;//模拟键盘
+            input[0].ki.wVk = (short)key;
+            input[0].ki.dwFlags = 0;//按下
+            SendInput(1u, input, Marshal.SizeOf((object)default(INPUT)));
+            Sleep(50);
+
+            input[0].type = 1;//模拟键盘
+            input[0].ki.wVk = (short)key;
+            input[0].ki.dwFlags = 2;//抬起
+            SendInput(1u, input, Marshal.SizeOf((object)default(INPUT)));
+            Sleep(50);
+        }
+
+        
+    }
+}
